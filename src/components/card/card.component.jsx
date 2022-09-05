ed4ee7dcda24ed6e35685245e242fa7028ea2331
Daniel Kim <danielkim2711@gmail.com>
2022-09-05 17:02:57 +1200

    Add status to card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,15 +3,28 @@ import { Link } from 'react-router-dom';
 import './card.styles.css';
 
 const Card = ({ job }) => {
+  const { id, title, clientName, clientEmail, createdAt, defaultStatus } = job;
+
   return (
-    <Link to={job.id} className='card-container'>
-      <h1>{job.title}</h1>
-      <div className='card-details'>
-        <p>{job.clientName}</p>
-        <span>|</span>
-        <p>{job.clientEmail}</p>
-        <span>|</span>
-        <p>{job.createdAt}</p>
+    <Link to={id} className='card-container'>
+      <h1>{title}</h1>
+      <div className='card-details-status-container'>
+        <div className='card-details'>
+          <p>{clientName}</p>
+          <span>|</span>
+          <p>{clientEmail}</p>
+          <span>|</span>
+          <p>{createdAt}</p>
+        </div>
+        <span
+          className={`card-status ${
+            defaultStatus === 'to priced'
+              ? 'card-status-to-priced'
+              : `card-status-${defaultStatus}`
+          }`}
+        >
+          {defaultStatus}
+        </span>
       </div>
     </Link>
   );
--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 const jobs = [
   {
     id: uuidv4(),
+    defaultStatus: 'scheduled',
     status: ['scheduled', 'active', 'invoicing', 'to priced', 'completed'],
     title: 'Install and maintain water supply systems',
     clientName: 'John Doe',
@@ -12,6 +13,7 @@ const jobs = [
   },
   {
     id: uuidv4(),
+    defaultStatus: 'active',
     status: ['scheduled', 'active', 'invoicing', 'to priced', 'completed'],
     title: 'Repair washing machines and sinks',
     clientName: 'John Doe',
@@ -21,12 +23,34 @@ const jobs = [
   },
   {
     id: uuidv4(),
+    defaultStatus: 'invoicing',
     status: ['scheduled', 'active', 'invoicing', 'to priced', 'completed'],
     title: 'Install waste disposal and sanitary systems',
     clientName: 'John Doe',
     clientEmail: 'jdoe@example.co.nz',
     contactPhone: '021 123 4567',
     createdAt: '03/09/2022',
+  },
+  {
+    id: uuidv4(),
+    defaultStatus: 'to priced',
+    status: ['scheduled', 'active', 'invoicing', 'to priced', 'completed'],
+    title: 'Install and maintain gas and liquid heating systems',
+    clientName: 'John Doe',
+    clientEmail: 'jdoe@example.co.nz',
+    contactPhone: '021 123 4567',
+    createdAt: '02/09/2022',
+  },
+  {
+    id: uuidv4(),
+    defaultStatus: 'completed',
+    status: ['scheduled', 'active', 'invoicing', 'to priced', 'completed'],
+    title:
+      'Inspecting and testing plumbing systems for safety, functionality, and code compliance',
+    clientName: 'John Doe',
+    clientEmail: 'jdoe@example.co.nz',
+    contactPhone: '021 123 4567',
+    createdAt: '01/09/2022',
   },
 ];
 
