2a8d301dda3f009417ea6d64f2491bb76202a5d2
Daniel Kim <danielkim2711@gmail.com>
2022-09-05 21:37:04 +1200

    Add status to job component and fetch job from db

--- src/routes/job/job.component.jsx
+++ src/routes/job/job.component.jsx
@@ -12,32 +12,47 @@ const Job = () => {
 
   return (
     <>
-      <div className='job-container'>
-        <Link to='/'>
-          <button className='btn-go-back'>
-            <IoArrowBack style={{ marginRight: '10px' }} /> Back
-          </button>
-        </Link>
-        <h1>Installing and maintaining water supply systems</h1>
-        <div className='job-description-container'>
-          <div className='job-description-left'>
-            <p>Client Name:</p>
-            <p>Location:</p>
-            <p>Client Email:</p>
-            <p>Contact Phone:</p>
-            <p>Date Published:</p>
-          </div>
-          <div className='job-description-right'>
-            <p>John Doe</p>
-            <p>123 Great Road, Newmarket, Auckland 1234</p>
-            <p>jdoe@example.co.nz</p>
-            <p>021 123 4567</p>
-            <p>05/09/2022</p>
-          </div>
-        </div>
+      {jobs
+        .filter((job) => job.id === jobId)
+        .map((job) => (
+          <div key={job.id} className='job-container'>
+            <Link to='/'>
+              <button className='btn-go-back'>
+                <IoArrowBack style={{ marginRight: '10px' }} /> Back
+              </button>
+            </Link>
+            <div className='job-title-container'>
+              <h1>{job.title}</h1>
+              <p
+                className={`job-status ${
+                  job.defaultStatus === 'to priced'
+                    ? 'job-status-to-priced'
+                    : `job-status-${job.defaultStatus}`
+                }`}
+              >
+                {job.defaultStatus}
+              </p>
+            </div>
+            <div className='job-description-container'>
+              <div className='job-description-left'>
+                <p>Client Name:</p>
+                <p>Location:</p>
+                <p>Client Email:</p>
+                <p>Contact Phone:</p>
+                <p>Date Published:</p>
+              </div>
+              <div className='job-description-right'>
+                <p>{job.clientName}</p>
+                <p>{job.location}</p>
+                <p>{job.clientEmail}</p>
+                <p>{job.contactPhone}</p>
+                <p>{job.createdAt}</p>
+              </div>
+            </div>
 
-        <NoteList />
-      </div>
+            <NoteList />
+          </div>
+        ))}
     </>
   );
 };
